3a745fa1ae69d955b7112df42094d29eb7ac20f1
Vinicius de Oliveira <vinicius.oliveira@itschimitz.com>
2023-02-13 11:28:14 -0300

    feat: contruindo a diagramação

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function FourOhFour() {
+    return (
+        <div className="mitz-notfound">
+            <Link href="/">
+                <img
+                alt="Mitz IT logo"
+                src="/playground_assets/mitzitbege1551-ij3l-200h.png"
+                className="mitz-logo-footer"
+                />
+            </Link>
+            <br></br>
+            <h3>404 - Página não encontrada</h3>
+            <Link href="/">
+                Ir para home
+            </Link>
+        </div>
+    )
+}
