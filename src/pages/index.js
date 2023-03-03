e50457012117852ac9085f02be837043990e6ae5
marcelors27 <marcelors27@gmail.com>
2023-03-03 01:41:38 -0300

    Add comma (#5)
    * chore: add comma
    
    * resolve merge main into fix
    
    * fix: PR workflow
    
    * fix: PR deploy to gh pages workflow
    
    * fix: PR deploy to gh pages workflow
    
    * fix: PR deploy to gh pages workflow
    
    * fix: only building on PR
    
    ---------
    
    Co-authored-by: Marcelo Schmitz <marceloschmitz@MacBook-Pro-de-Marcelo.local>
    Co-authored-by: lgcmotta <luiz.motta@itschmitz.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ const HomePage = (props) => {
 
           <span className="mitz-title-header">
             <h1>
-              Soluções de precisão<br />
+              Soluções de precisão,<br />
               Qualidade de ponta a ponta
             </h1>
             <h5>
